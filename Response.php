e31b0374c7c241c6d55ccb86fb2e96bba2cbe3d9
Alexandr Viniychuk <alexandr.viniychuk@icloud.com>
2014-10-20 19:08:43 +0300

    in development

---
+++ Response.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is a part of Solve framework.
+ *
+ * @author Alexandr Viniychuk <alexandr.viniychuk@icloud.com>
+ * @copyright 2009-2014, Alexandr Viniychuk
+ * created: 10/20/14 7:07 PM
+ */
+
+namespace Solve\Http;
+
+/**
+ * Class Response
+ * @package Solve\Http
+ *
+ * Class Response is used to generate correct response from application
+ *
+ * @version 1.0
+ * @author Alexandr Viniychuk <alexandr.viniychuk@icloud.com>
+ */
+class Response {
+
+}
